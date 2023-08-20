2d635fee56989fb124cfbad312873516eb47527b
unknown <little_black@sjtu.edu.cn>
2023-08-20 15:37:59 +0800

    merge css files, align the right bar

--- public/js/hover.js
+++ public/js/hover.js
@@ -3,7 +3,7 @@ const siderbar_right_after_div = document.getElementById("hover_after");
 
 siderbar_right_div.addEventListener("mouseover", function(){
     siderbar_right_div.style.width = "10rem";
-    siderbar_right_div.style.height = "20rem";
+    siderbar_right_div.style.height = "16rem";
     setTimeout(() => {
         siderbar_right_div.style.display = "none";
         siderbar_right_after_div.style.display = "inline-block";
@@ -15,7 +15,7 @@ siderbar_right_after_div.addEventListener("mouseleave", function(){
     siderbar_right_div.style.display = "inline-block";
     siderbar_right_after_div.style.display = "none";
     setTimeout(() => {
-        siderbar_right_div.style.width = "5rem";
-        siderbar_right_div.style.height = "1rem";
+        siderbar_right_div.style.width = "6rem";
+        siderbar_right_div.style.height = "2rem";
     }, 300);
 });
