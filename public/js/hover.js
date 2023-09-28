91572f13d16e5eddb8af01827d54ae86c4313443
unknown <little_black@sjtu.edu.cn>
2023-09-28 23:50:41 +0800

    feat: finish publications

--- public/js/hover.js
+++ public/js/hover.js
@@ -2,7 +2,7 @@ const siderbar_right_div = document.getElementById("hover_before");
 const siderbar_right_after_div = document.getElementById("hover_after");
 
 siderbar_right_div.addEventListener("mouseover", function(){
-    siderbar_right_div.style.width = "5rem";
+    siderbar_right_div.style.width = "6rem";
     siderbar_right_div.style.height = "8rem";
     setTimeout(() => {
         siderbar_right_div.style.display = "none";
@@ -15,7 +15,7 @@ siderbar_right_after_div.addEventListener("mouseleave", function(){
     siderbar_right_div.style.display = "inline-block";
     siderbar_right_after_div.style.display = "none";
     setTimeout(() => {
-        siderbar_right_div.style.width = "5rem";
+        siderbar_right_div.style.width = "6rem";
         siderbar_right_div.style.height = "2rem";
     }, 300);
 });
